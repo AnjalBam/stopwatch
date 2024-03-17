83676a5d0ace957299d3c5dc8f4678d9b4399b9c
Anjal Bam <anjalbam8181@gmail.com>
2024-03-17 18:22:04 +0000

    fix: remove default open of dropdown:

--- app/components/TopBarActions.tsx
+++ app/components/TopBarActions.tsx
@@ -11,7 +11,7 @@ type ThemeOptionType = {
 };
 
 export function TopBarActions({ changeClockStyle, isMuted = false }: any) {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   const ThemeOptions: ThemeOptionType[] = [
     {
