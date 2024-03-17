f46174ec6aa97908096a4bdba266d58afa911c34
Anjal Bam <anjalbam8181@gmail.com>
2024-03-17 18:20:49 +0000

    feat: update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,8 @@ export const metadata: Metadata = {
   title: "Timer - Anjal Bam",
   description:
     "Introducing a minimalist timer app designed to keep you focused without the distractions. Whether you're working on a project, studying for an exam, or practicing mindfulness, this timer is here to help you stay on track without any unnecessary distractions.",
-  keywords: "clock, timer, stopwatch",
+  keywords:
+    "clock, timer, stopwatch, minimalist, aesthetic, productivity,minimalist timer, productivity tool, focus app, distraction-free timer, time management, minimalist design, productivity app, work timer, study timer, mindfulness timer, productivity tool, time tracking, minimalist productivity, simple timer, browser timer",
 };
 
 export default function RootLayout({
